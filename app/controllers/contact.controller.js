3e3d304190b020014dff85af081914b68a83e5ee
huynhhonganh <honganhtaap@gmail.com>
2024-03-06 21:40:24 +0700

    Cai dat express chay thong bao xin chao

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (rep, res) => {
+    res.send({message: "create handler"});
+};
+
+exports.findALL = (rep, res) => {
+    res.send({message: "findALL handler"});
+};
+
+exports.findOne = (rep, res) => {
+    res.send({message: "findOne handler"});
+};
+
+exports.Update = (rep, res) => {
+    res.send({message: "Update handler"});
+};
+
+exports.delete = (rep, res) => {
+    res.send({message: "delete handler"});
+};
+
+exports.deleteALL = (rep, res) => {
+    res.send({message: "deleteALL handler"});
+};
+
+exports.findALLFavorite = (rep, res) => {
+    res.send({message: "findALLFavorite handler"});
+};
